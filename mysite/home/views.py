3675faa92202096afe11922aea362fe97ce88135
oThiagoSouza <souzaalvesthiago99@gmail.com>
2025-03-22 00:08:00 -0300

    Creating requirements.txt

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -3,8 +3,16 @@ from django.http import HttpResponse
 
 
 def ver_home(request):
-    return render(request, 'home.html')
-
+    if request.method == 'GET':
+        return render(request, 'home.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        senha = request.POST.get('senha')
+        
+        if nome != nome and senha != senha:
+            HttpResponse('Usuário ou senha incorreto')
+        else:
+            return HttpResponse('Usuário e senha corretos')
 
 def ver_home2(request):
     return HttpResponse('Olá, mundo 2!')
